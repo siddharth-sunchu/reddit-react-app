6c5413dbe3fd1b49e03755cf20ffd3c7552351ce
siddharth-sunchu <siddharth.sunchu@officedepot.com>
2021-06-14 00:32:01 -0400

    added test case for container

--- src/containers/SearchBar/SearchBarContainer.test.jsx
+++ src/containers/SearchBar/SearchBarContainer.test.jsx
@@ -4,9 +4,8 @@ import { SearchBarContainer } from 'containers';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-// jest.mock('components', () => () => 'PostCards');
-// jest.mock('components', () => () => 'Loader');
-// jest.mock('components', () => () => 'PageNav');
+import * as api from 'api/posts/fetchPosts';
+import * as utils from 'utils';
 
 import { mockSearchStore, mockPostStore } from 'mocks';
 describe('<SearchBarContainer />', () => {
@@ -18,11 +17,62 @@ describe('<SearchBarContainer />', () => {
   });
 
   it('Renders', () => {
+    const store = mockReduxStore({
+      posts: mockPostStore,
+      search: mockSearchStore
+    });
     const wrapper = mount(
       <Provider store={store}>
         <SearchBarContainer />
       </Provider>
     );
     expect(wrapper.exists()).toBe(true);
+  });
+
+  it('On Click Button action', () => {
+    const store = mockReduxStore({
+      posts: mockPostStore,
+      search: mockSearchStore
+    });
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <SearchBarContainer />
+      </Provider>
+    );
+    const button = wrapper.find('button');
+    button.simulate('click');
+    const expectedActions = [
+      { type: 'CLEAR_POSTS' },
+      { type: 'IS_LOADING' },
+      { type: 'SET_RESULT_TERM', payload: '' }
+    ];
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it('On Change action', () => {
+    const store = mockReduxStore({
+      posts: mockPostStore,
+      search: mockSearchStore
+    });
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <SearchBarContainer />
+      </Provider>
+    );
+    const input = wrapper.find('input');
+    input.simulate('change', {
+      target: {
+        value: 'test'
+      }
+    });
+    const expectedActions = [
+      { type: 'CLEAR_POSTS' },
+      { type: 'SET_RESULT_TERM', payload: '' },
+      { type: 'UPDATE_SEARCH_TERM', payload: 'test' }
+    ];
+
+    expect(store.getActions()).toEqual(expectedActions);
   });
 });
